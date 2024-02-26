7d06099cca3c3132a057fc2a5fd315b13627991e
Jordy Parion <jj.pariona@uea.edu.ec>
2024-02-25 19:28:05 -0500

    deber 11

---
+++ PROGRAMA 2.py
@@ -0,0 +1,11 @@
+matriz = [
+    [50, 40, 19],
+    [68, 80, 58],
+    [45, 36, 72]
+]
+
+for fila in matriz:
+    #print(fila)
+    #fila.sort(reverse=True)
+    #print(fila)
+    print(sorted(fila))
